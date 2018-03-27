faa6b64cd93a769474fee908df770977aca9108e
Ilya Konon <ilya.konon.95@gmail.com>
2018-03-27 09:53:21 +0000

    add: week1, 1st block of tasks.

---
+++ week1/penguins_print.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+head = '   _~_    '
+eyes = '  (o o)   '
+beak = ' /  V  \  '
+body = '/(  _  )\ '
+foot = '  ^^ ^^   '
+
+print(head * num, eyes * num, beak * num, body * num, foot * num, sep='\n')
