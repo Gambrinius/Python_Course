77a55a407f16d9c19ca5c3e43fbe131809fa1ea8
Gambrinius <ilya.konon.95@gmail.com>
2018-03-27 22:51:53 +0300

    add: finished 3rd block of task for week 1

--- week1/sym_number.py
+++ week1/sym_number.py
@@ -1,5 +1,11 @@
 number = int(input())
-first_digits = number % 100
-second_2digits = number // 100
-print(first_digits)
-print(second_2digits)
+k1 = 1
+k2 = 2
+k3 = 3
+
+n3 = (number // 10 ** k1) % 10
+n2 = (number // 10 ** k2) % 10
+n1 = (number // 10 ** k3) % 10
+n4 = (number % 10 ** k1)
+
+print(n1 == n4 and n2 == n3 and 1 or 2)
